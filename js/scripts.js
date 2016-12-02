49526a4614b974d3b48ae4c5e4f3cff3f1090a51
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-12-02 16:55:57 -0600

    progress on game'

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,48 @@
 
-var strictMode;
-var twentyCount;
+var game = {
+  count: 0,
+  colors: ['green', 'red', 'blue', 'yellow'],
+  computer: [],
+  player: [],
+  sound:{
+    green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'), 
+    red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'), 
+    blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'), 
+    yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+  },
+  strictMode: false,
+}
+
+function newGame(){
+    game.count = 0;
+    game.computer = [];
+    game.player = [];
+    game.strictMode = false;
+}
+
+function computerMove(){
+    var select = Math.floor((Math.random() * 4) + 1);
+    var compColor = game.colors[select];
+    console.log(compColor);
+    game.computer.push(game.colors[select]);
+    game.sound[compColor].play();
+}
+
+
+
+
+
+
+
+
+
 
 function showSecondPanel() {
     $('.panel-2').show();
 }
 function showThirdPanel() {
     $('.game, .start-win-panel').show();
+
 }
 
 
@@ -18,16 +54,22 @@ $(document).ready(function () {
 
     $('.panel-2 a').click(function () {
         var id = this.id;
-        strictMode = id === 'strict-mode-selection' ? true : false; // if Strict Mode strictMode is true
+        game.strictMode = id === 'strict-mode-selection' ? true : false; // if Strict Mode strictMode is true
         $('.panel-2').hide('drop', { direction: 'left' }, 1000, showThirdPanel);
     });
     $('#startWin').click(function () {
-       $('.start-win-panel').hide();
+        $('.start-win-panel').hide();
     });
 
+    var green = $("#control-top-left").data("color");
+    console.log(green);
 
+    $('#control-top-left').click(computerMove);
 
 
 
 
 });
+
+
+//https://medium.com/front-end-hacking/create-simon-game-in-javascript-d53b474a7416#.psrjgd2qu
