9414e7e71c1caa8df9388a43609d61e057579587
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-12-02 12:01:55 -0600

    setup game

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function showSecondPanel() {
     $('.panel-2').show();
 }
 function showThirdPanel() {
-    $('.game').show();
+    $('.game, .start-win-panel').show();
 }
 
 
@@ -15,11 +15,14 @@ $(document).ready(function () {
     $('.panel-1 a').click(function () {
         $('.panel-1').hide('drop', { direction: 'left' }, 1000, showSecondPanel);
     });
-    
+
     $('.panel-2 a').click(function () {
         var id = this.id;
         strictMode = id === 'strict-mode-selection' ? true : false; // if Strict Mode strictMode is true
         $('.panel-2').hide('drop', { direction: 'left' }, 1000, showThirdPanel);
+    });
+    $('#startWin').click(function () {
+       $('.start-win-panel').hide();
     });
 
 
